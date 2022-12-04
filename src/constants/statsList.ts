a4b18c0345b963cc61d0a5d56204c03cce93c4a8
italoScript <italoyoupw@gmail.com>
2022-12-03 23:51:22 -0300

    feat: adding constants colors and labels for pokemons info

---
+++ src/constants/statsList.ts
@@ -0,0 +1,33 @@
+
+export const statsList:any = {
+    hp:{
+        color: 'danger',
+        name: 'HP',
+        barStyle:null
+    },
+    attack:{
+        color: 'warning',
+        name: 'Attack',
+        barStyle:null
+    },
+    defense:{
+        color: 'success',
+        name: 'Defense',
+        barStyle:null
+    },
+    'special-attack':{
+        color: 'info',
+        name: 'Special Attack',
+        barStyle:null
+    },
+    'special-defense':{
+        color: 'primary',
+        name: 'Special Defense',
+        barStyle:null
+    },
+    speed:{
+        color: 'secondary',
+        name: 'Speed',
+        barStyle:null
+    },
+}
