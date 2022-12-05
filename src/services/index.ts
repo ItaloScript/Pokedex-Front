df6166747bab441bf27ed9a3fd16f1c4b504f099
italoScript <italoyoupw@gmail.com>
2022-12-05 04:05:18 -0300

    fix: adding api url to baseURL

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,7 @@
 //create service api using axios
 import axios from 'axios'
 const service = axios.create({
-    baseURL:  import.meta.env.VITE_BACKEND_URL || 'http:///192.168.2.102:3000/',
+    baseURL:  import.meta.env.VITE_BACKEND_URL || 'https://pokedex-italo.up.railway.app',
 })
 
 export default service
