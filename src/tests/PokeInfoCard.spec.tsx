487dd801678aa6162502aa2770ded85023922a83
italoScript <italoyoupw@gmail.com>
2022-12-05 04:59:33 -0300

    test: adjusting PokeInfoCard tests

--- src/tests/PokeInfoCard.spec.tsx
+++ src/tests/PokeInfoCard.spec.tsx
@@ -1,4 +1,5 @@
-import {  render, waitFor } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
+import * as React from 'react';
 import { PokeInfoCard } from '../components/PokeInfoCard';
 
 jest.setTimeout(10000)
@@ -6,27 +7,26 @@ jest.setTimeout(10000)
 describe(' PokeInfoCard Component', () => {
 
     it('should render poke info', async () => {
-        const {  findByText} = render(<PokeInfoCard idPokemon={1}/>);
+        const { findByText} = render(<PokeInfoCard onClose={() => {}} isOpen={true} idPokemon={1} />);
         await waitFor(async () => {
-            expect(await findByText('bulbasaur')).toBeInTheDocument();
-        }, {timeout: 10000});
+            expect(await findByText('Sobre')).toBeInTheDocument();
+        }, { timeout: 10000 });
+
     });
-    
+
     it('shoud render poke info with correct image', async () => {
-        const {  findByTestId} = render(<PokeInfoCard idPokemon={1}/>);
+        const { findByTestId } = render(<PokeInfoCard onClose={() => {}} isOpen={true} idPokemon={1} />);
         const image = await waitFor(async () => {
             return await findByTestId('poke-info-image')
-        }, {
-            timeout: 10000
-        });
+        }, {timeout: 10000});
         expect(image).toHaveAttribute('src', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png')
     });
 
     it('should render poke description', async () => {
-        const {  findByText} = render(<PokeInfoCard idPokemon={1}/>);
+        const { findByText} = render(<PokeInfoCard onClose={() => {}} isOpen={true} idPokemon={1} />);
         await waitFor(async () => {
             expect(await findByText(`A Strange Seed Was Planted On Its Back At Birth. The Plant Sprouts And Grows With This Pokemon.`)).toBeInTheDocument();
-        }, {timeout: 10000})
+        }, { timeout: 10000 })
     })
-        
+
 })
