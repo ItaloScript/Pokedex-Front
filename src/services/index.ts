dc09e5729ec4fdaef34be53bef733ae7139d31bc
italoScript <italoyoupw@gmail.com>
2022-12-05 05:09:24 -0300

    fix: process is not defined bug

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,7 @@
 //create service api using axios
 import axios from 'axios'
 const service = axios.create({
-    baseURL:  process.env.VITE_BACKEND_URL || 'https://pokedex-italo.up.railway.app',
+    baseURL:  'https://pokedex-italo.up.railway.app',
 })
 
 export default service
